877230d336e9ce8221f32175db3a82c97c820034
AWSafran <andrewsafran@gmail.com>
2019-05-13 15:40:47 -0400

    added findcoursebyid method to course service and set up files for testing

--- school/src/main/java/com/lambdaschool/school/service/CourseServiceImpl.java
+++ school/src/main/java/com/lambdaschool/school/service/CourseServiceImpl.java
@@ -43,4 +43,16 @@ public class CourseServiceImpl implements CourseService
             throw new EntityNotFoundException(Long.toString(id));
         }
     }
+    
+    @Override
+    public Course findCourseById(long id) throws EntityNotFoundException
+    {
+        if(courserepos.findById(id).isPresent())
+        {
+            return courserepos.findById(id).get();
+        }else
+        {
+            throw new EntityNotFoundException();
+        }
+    }
 }
